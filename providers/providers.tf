94a95bc7f6b3a53c039eb86c6c01ab333d857028
aquiles.craane@gmail.com <aquiles.craane@gmail.com>
2022-09-27 15:53:33 +0200

    removed TLS in provider. and changed name deployment

--- providers/providers.tf
+++ providers/providers.tf
@@ -10,13 +10,9 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
-    }
   }
 }
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
